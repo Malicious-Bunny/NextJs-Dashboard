b520022987563aee24e700138343a95159713f3d
Malicious-Bunny <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2024-01-12 18:01:50 +0100

    Ready to add DB

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return (
+        <div className=" text-center flex flex-col justify-center h-screen bg-black text-white subpixel-antialiased">
+            <h1>Customers</h1>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return (
+        <div className=" flex-1  text-center flex flex-col justify-center h-screen bg-black text-white subpixel-antialiased">
+            <h1>Invoices</h1>
+        </div>
+    )
+}
